4e0bed6f1dd1a01554b5815e0685ee9518f46bf7
Stephanie Ta <stephaniecarlynta@gmail.com>
2024-04-01 19:51:43 -0700

    fix syntax issues

--- src/replace_value.py
+++ src/replace_value.py
@@ -44,18 +44,18 @@ def replace_value(dataframe, column, old_value, new_value):
         raise TypeError("You are not using a pandas DataFrame for the dataframe input")
     if type(column) is not str:
         raise TypeError("You are not using a string for the column input")
-    if type(old_value) is not in [str, int, float]:
+    if type(old_value) not in [str, int, float]:
         raise TypeError("You are not using a string, integer, or float for the old_value input")
-    if type(new_value) is not in [str, int, float]:
+    if type(new_value) not in [str, int, float]:
         raise TypeError("YYou are not using a string, integer, or float for the new_value input")
     
     # check if the specifed column is in the inputted pandas DataFrame
-    if column is not in dataframe.columns:
+    if column not in dataframe.columns:
         raise Exception("The specified column is not in the specified dataframe")
 
     # check if old_value is in the specified column
-    if old_value is not in dataframe[column].values:
+    if old_value not in dataframe[column].values:
         raise Exception("The specified old_value is not in the specified column")
 
-    new_dataframe = (dataframe.loc[dataframe[column] == old_value, column] = new_value)
-    return new_dataframe
+    dataframe.loc[dataframe[column] == old_value, column] = new_value
+    return dataframe
