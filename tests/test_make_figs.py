5c01e2a091da1460bd061f7402f3f55217e359cb
Siddharth Balodi <116980792+Ballods@users.noreply.github.com>
2024-04-04 22:39:10 -0700

    Update test_make_figs.py

--- tests/test_make_figs.py
+++ tests/test_make_figs.py
@@ -6,7 +6,7 @@ import pandas as pd
 # Import make_histogram function from src folder
 sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
 from src.make-figs import plot_chart
-from src.08-make-figs import plot4
+from src.08_make-figs import plot4
 
 # Mock data for testing
 data = pd.DataFrame({
