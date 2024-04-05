e44936a8f9ff5685036c08c4512f203ddaf694fd
Siddharth Balodi <116980792+Ballods@users.noreply.github.com>
2024-04-04 22:46:04 -0700

    Updated make_figs.py to include implementation for plot4

--- src/make_figs.py
+++ src/make_figs.py
@@ -25,3 +25,24 @@ def plot_chart(data, x_field, title, prediction_field, actual_field, save_path):
 
     plt = (prediction_chart | actual_chart).resolve_scale(color='independent')
     plt.save(save_path)
+
+
+def plot4(data):
+    alt.data_transformers.disable_max_rows()
+
+    melt_flight_predict = data.melt(id_vars=['MONTH', 'DAY_OF_WEEK', 'DEP_DEL15', 'CARRIER_NAME', 'prediction', 'index'])
+
+    dropdown_options = ['CONCURRENT_FLIGHTS', 'FLT_ATTENDANTS_PER_PASS', 'GROUND_SERV_PER_PASS', 'PLANE_AGE', 'SNOW', 'AWND']
+    dropdown_numeric_variable = alt.binding_select(options=dropdown_options, name='Y-axis feature')
+    selection = alt.selection_point(fields=['variable'], bind=dropdown_numeric_variable)
+
+    drop_down_chart = alt.Chart(melt_flight_predict, width=1000, height=400
+                                    ).mark_circle(opacity=0.4).encode(
+                                        y=alt.X('value:Q', title=""),
+                                        x=alt.Y('MONTH', title="Month", sort=["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]),
+                                        color=alt.Color('prediction', title="Model Prediction")
+                                    ).add_params(selection).transform_filter(selection)
+
+    drop_down_chart.save("results/07_fig_numeric-feats-interactive-viz.html")
+
+
