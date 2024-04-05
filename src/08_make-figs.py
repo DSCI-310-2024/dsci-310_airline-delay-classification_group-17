cca912029ca52a2bdf0d17a13a157133040f45f9
Siddharth Balodi <116980792+Ballods@users.noreply.github.com>
2024-04-04 22:23:19 -0700

    Update 08_make-figs.py

--- src/08_make-figs.py
+++ src/08_make-figs.py
@@ -4,11 +4,11 @@ import numpy as np
 import sklearn
 
 from sklearn.metrics import  confusion_matrix, ConfusionMatrixDisplay, classification_report
-from load_and_save import load_data
+from make-figs import plot_chart
 
 def main():
-    predictions = load_data("results/03_knn-test-predict.csv")
-    flight_test = load_data("data/processed/02_flight-test.csv").reset_index()
+    predictions = pd.read_csv("results/03_knn-test-predict.csv")
+    flight_test = pd.read_csv("data/processed/02_flight-test.csv").reset_index()
 
     flight_test_predict = pd.concat([flight_test, predictions], axis=1)
 
@@ -24,77 +24,13 @@ def main():
     plot4(flight_test_predict)
 
 def plot1(data):
-    month_vs_prediction = alt.Chart(data, width=300, height=200, title="Month and Predicted Flight Delay"
-                                    ).mark_bar().encode(
-                                        x=alt.X("MONTH", title="Month", axis=alt.Axis(labelAngle=0),
-                                                sort=["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]),
-                                        xOffset="prediction",
-                                        y=alt.Y("count()", title="Number of Flights"),
-                                        color=alt.Color("prediction", title="Model Prediction"),
-                                        tooltip=alt.Tooltip(["count()", "MONTH"])
-                                    )
-
-    month_vs_real = alt.Chart(data, width=300, height=200, title="Month and Actual Flight Delay"
-                                    ).mark_bar().encode(
-                                        x=alt.X("MONTH", title="Month", axis=alt.Axis(labelAngle=0),
-                                                sort=["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]),
-                                        xOffset="DEP_DEL15",
-                                        y=alt.Y("count()", title="Number of Flights"),
-                                        color=alt.Color("DEP_DEL15", title="Actual"),
-                                        tooltip=alt.Tooltip(["count()", "MONTH"])
-                                    )
-
-    plt = (month_vs_prediction | month_vs_real).resolve_scale(color='independent')
-
-    plt.save("results/04_fig_month-vs-prediction-actual.png")
+    plot_chart(data, "MONTH", "Month and Predicted Flight Delay", "prediction", "DEP_DEL15", "results/04_fig_month-vs-prediction-actual.png")
 
 def plot2(data):
-    day_vs_prediction = alt.Chart(data, width=300, height=250, title="Day of the Week and Predicted Flight Delay"
-                                    ).mark_bar().encode(
-                                        x=alt.X("DAY_OF_WEEK", title="Day of the Week", axis=alt.Axis(labelAngle=0),
-                                                sort=["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"]),
-                                        xOffset="prediction",
-                                        y=alt.Y("count()", title="Number of Flights"),
-                                        color=alt.Color("prediction", title="Model Prediction"),
-                                        tooltip=alt.Tooltip(["count()", "DAY_OF_WEEK"])
-                                    )
-
-    day_vs_real = alt.Chart(data, width=300, height=250, title="Day of the Week and Actual Flight Delay"
-                                    ).mark_bar().encode(
-                                        x=alt.X("DAY_OF_WEEK", title="Day of the Week", axis=alt.Axis(labelAngle=0),
-                                                sort=["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"]),
-                                        xOffset="DEP_DEL15",
-                                        y=alt.Y("count()", title="Number of Flights"),
-                                        color=alt.Color("DEP_DEL15", title="Actual"),
-                                        tooltip=alt.Tooltip(["count()", "DAY_OF_WEEK"])
-                                    )
-
-    plt2 = (day_vs_prediction | day_vs_real).resolve_scale(color='independent')
-
-    plt2.save("results/05_fig_day-vs-prediction-actual.png")
+    plot_chart(data, "DAY_OF_WEEK", "Day of the Week and Predicted Flight Delay", "prediction", "DEP_DEL15", "results/05_fig_day-vs-prediction-actual.png")
 
 def plot3(data):
-    carrier_vs_prediction = alt.Chart(data, width=400, height=300, title="Flight Carriers and Predicted Flight Delay"
-                                        ).mark_bar().encode(
-                                            x=alt.X("CARRIER_NAME", title="Airline Carrier", axis=alt.Axis(labelAngle=-45)),
-                                            xOffset="prediction",
-                                            y=alt.Y("count()", title="Number of Flights"),
-                                            color=alt.Color("prediction", title="Model Prediction"),
-                                            tooltip=alt.Tooltip(["count()", "CARRIER_NAME"])
-                                        )
-
-    carrier_vs_real = alt.Chart(data, width=400, height=300, title="Flight Carriers and Actual Flight Delay"
-                                        ).mark_bar().encode(
-                                            x=alt.X("CARRIER_NAME", title="Airline Carrier", axis=alt.Axis(labelAngle=-45)),
-                                            xOffset="DEP_DEL15",
-                                            y=alt.Y("count()", title="Number of Flights"),
-                                            color=alt.Color("DEP_DEL15", title="Actual"),
-                                            tooltip=alt.Tooltip(["count()", "CARRIER_NAME"])
-                                        )
-
-    plt3 = (carrier_vs_prediction | carrier_vs_real).resolve_scale(color='independent')
-
-    plt3.save("results/06_fig_carrier-vs-prediction-actual.png")
+    plot_chart(data, "CARRIER_NAME", "Flight Carriers and Predicted Flight Delay", "prediction", "DEP_DEL15", "results/06_fig_carrier-vs-prediction-actual.png")
 
 def plot4(data):
     alt.data_transformers.disable_max_rows()
@@ -115,4 +51,4 @@ def plot4(data):
     drop_down_chart.save("results/07_fig_numeric-feats-interactive-viz.html")
 
 if __name__ == "__main__":
-    main()
+    main
